a17b94cd59d40fb80f3f6c7bd1383dd52e8f83b2
treeps <treeps@student.42wolfsburg.de>
2023-07-18 12:14:33 +0200

    refined test render

--- src/test_render.c
+++ src/test_render.c
@@ -14,43 +14,37 @@
 
 void	test_render(t_data *data)
 {
-	int	x;
-	int	y;
+	int		x;
+	int		y;
+	t_vec2	b;
 
 	clear_screen(data);
 	draw_box(data);
 	x = data->player.pos.x + WIDTH / 2;
 	y = data->player.pos.y + HEIGHT / 2;
 	draw_player(data, x, y);
-	draw_line(data->player.pos, vec2_mul(vec2_add(data->player.pos, data->player.dir), 5.0), data);
+	b = vec2_mul(data->player.dir, 25.0);
+	b = vec2_add(data->player.pos, b);
+	x = b.x + WIDTH / 2;
+	y = b.y + HEIGHT / 2;
+	my_mlx_pixel_put(data, x, y, 0x00FF0000);
 }
 
 void	draw_box(t_data *data)
 {
 	int	i;
-	int	j;
 
-	j = 10;
-	while (j < HEIGHT - 10)
-	{
-		my_mlx_pixel_put(data, 10, j, 0x00FFFFFF);
-		j++;
-	}
-	j = 10;
-	while (j < HEIGHT - 10)
-	{
-		my_mlx_pixel_put(data, WIDTH - 10, j, 0x00FFFFFF);
-		j++;
-	}
 	i = 10;
-	while (i < WIDTH - 10)
+	while (i < HEIGHT - 10)
 	{
-		my_mlx_pixel_put(data, i, 10, 0x00FFFFFF);
+		my_mlx_pixel_put(data, 10, i, 0x00FFFFFF);
+		my_mlx_pixel_put(data, WIDTH - 10, i, 0x00FFFFFF);
 		i++;
 	}
 	i = 10;
 	while (i < WIDTH - 10)
 	{
+		my_mlx_pixel_put(data, i, 10, 0x00FFFFFF);
 		my_mlx_pixel_put(data, i, HEIGHT - 10, 0x00FFFFFF);
 		i++;
 	}
@@ -72,11 +66,6 @@ void	draw_player(t_data *data, int x, int y)
 		}
 		i++;
 	}
-}
-
-void	draw_line(t_vec2 a, t_vec2 b, t_data *data)
-{
-
 }
 
 void	clear_screen(t_data *data)
